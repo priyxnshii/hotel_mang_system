fcf783665d550e615db437e550ea734cf31859b5
priyxnshii <priyanshidb2003@gmail.com>
2025-01-02 18:56:02 +0530

    Add files via upload

---
+++ Splash.java
@@ -0,0 +1,31 @@
+package hotel.management.system;
+
+import javax.swing.*;
+public class Splash extends JFrame{
+    Splash()
+    {
+        ImageIcon imageIcon =new ImageIcon(ClassLoader.getSystemResource("icons/splash.gif"));
+        JLabel label=new JLabel(imageIcon);
+        label.setBounds(0,0,858,680);
+        add(label);
+
+        setLayout(null);
+        setLocation(300,100);
+        setSize(858,680);
+        setVisible(true);
+
+        try{
+            Thread.sleep(5000);
+            setVisible(false);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
